a0bd9568397580e5accd0b3912939072edabdafa
Sreenath <sreenathsrp256@gmail.com>
2024-01-10 19:46:55 +0530

    edited the base url

--- Front-end/utils/axios.js
+++ Front-end/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http:',
+  baseURL: 'https://contact-manager-smoky.vercel.app/api/',
   headers: {
     'Content-Type': 'application/json',
     // You can add other default headers here
