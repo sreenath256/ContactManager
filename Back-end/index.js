975c227a9b703700483d9af4dc87e0ae91290e60
Sreenath <sreenathsrp256@gmail.com>
2024-01-10 19:40:26 +0530

    testing code

--- Back-end/index.js
+++ Back-end/index.js
@@ -9,12 +9,13 @@ connectDb();
 
 const port = process.env.PORT || 5001;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-
+app.use("/api/test",require("./routes/testRouter"))
 app.use("/api/contacts", require("./routes/contactRouter"));
 app.use("/api/user", require("./routes/userRouter"));
+
 
 
 app.use(errorHandling);
--- Front-end/utils/axios.js
+++ Front-end/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://contact-manager-jfe39ppqc-sreenath256.vercel.app/api/',
+  baseURL: 'http:',
   headers: {
     'Content-Type': 'application/json',
     // You can add other default headers here
